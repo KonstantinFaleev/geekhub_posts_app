544f8f51a245aebea7f99e2a8b99efcdb5329190
KonstantinFaleev <konstantin.faleev@gmail.com>
2017-12-11 00:18:31 +0200

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
-  resources :posts,          only: [:create, :destroy]
+  resources :posts,               only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
